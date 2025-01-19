f1142a77e47e34a68d874787e3087a0daf61014a
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-19 02:54:16 +0000

    feat: organiza e configura a estrutura inicial do projeto.
    - Configura o arquivo pom.xml com as dependências principais.
    - Adiciona o arquivo .gitignore com regras apropriadas.
    - Configura o banco de dados H2 no application.properties.
    - Define a estrutura inicial das pastas e o ponto de entrada HelpChainApplication.java.

---
+++ src/main/java/com/helpchain/HelpChainApplication.java
@@ -0,0 +1,13 @@
+package com.helpchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpChainApplication.class, args);
+        System.out.println("HelpChain está rodando! 🚀");
+    }
+}
