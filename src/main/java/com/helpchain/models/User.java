a3c9c5daeeba4fadbbc654ddf82535b944dc47ff
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-19 03:05:56 +0000

    feat: cria entidade User para representar usuários do sistema

---
+++ src/main/java/com/helpchain/models/User.java
@@ -0,0 +1,27 @@
+package com.helpchain.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data 
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; 
+
+    
+}
